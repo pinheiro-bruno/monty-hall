41dab54e8c84d4580159016d53c12833b747dbd3
Bruno Pinheiro <brunosantiagopinheiro@gmail.com>
2020-02-26 23:03:34 -0300

    adds cards options and randomize it

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        content: {{ content }} <br>
+        selected: {{ selected }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['content', 'selected']
+}
+</script>
+
+<style scoped>
+  .card{
+    margin-bottom: 20px;
+  }
+</style>
--- src/components/MontyHall.vue
+++ src/components/MontyHall.vue
@@ -1,14 +1,53 @@
 <template>
-  <div class="monty-hall"></div>
+  <div class="monty-hall">
+    <card v-bind:content="card.content"
+      v-bind:selected="card.selected"
+      v-for="card in cards"
+      v-bind:key="card.id"
+    ></card>
+  </div>
 </template>
 
 <script>
+import Card from '@/components/Card'
+import _ from 'underscore'
+
+const GOAT = 'goat'
+const CAR = 'car'
+
 export default {
   name: 'MontyHall',
+  components: {
+    Card
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      cards: [
+        {
+          id: 1,
+          content: GOAT,
+          selected: false
+        },
+        {
+          id: 2,
+          content: GOAT,
+          selected: false
+        },
+        {
+          id: 3,
+          content: CAR,
+          selected: false
+        }
+      ]
     }
+  },
+  methods: {
+    shuffleCards: function () {
+      this.cards = _.shuffle(this.cards)
+    }
+  },
+  beforeMount () {
+    this.shuffleCards()
   }
 }
 </script>
