4f7881b83fa37c5599b9385bba01b489940dc037
Bruno Pinheiro <brunosantiagopinheiro@gmail.com>
2020-03-02 09:22:51 -0300

    makes the game playable, not recording answers

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-col md="2" :class="{ selected: selected }" class="card" @click="select">
+    <div v-show="revealed" class="content">{{ content }}</div>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['id', 'selected', 'content', 'revealed'],
+  methods: {
+    select () {
+      this.$emit('cardSelect', this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    height: 270px;
+    line-height: 270px;
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+
+  .selected {
+    border: 1px green solid;
+  }
+</style>
